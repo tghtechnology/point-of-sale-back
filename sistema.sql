a5c1466db39fa50a9a9f55a5f0d8a0e953c25c82
JimenaAgustinNavarro <jimenaagustin0@gmail.com>
2024-03-11 23:30:09 -0500

    agregado columna eliminado_fecha_temporal a Usuario

--- sistema.sql
+++ sistema.sql
@@ -159,6 +159,7 @@ CREATE TABLE `usuarios` (
   `password` varchar(255) NOT NULL,
   `pais` varchar(255) DEFAULT NULL,
   `estado` boolean not null,
+  `eliminado_temporal_fecha` TIMESTAMP DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
