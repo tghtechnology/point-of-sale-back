b3570732e33b84aed6a84b5badd6ecb061b077c1
Estefany Torres <estefanyt3101@gmail.com>
2024-03-17 19:41:29 -0500

    new

--- prisma/migrations/20240315013930_sistema_venta/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `eliminadoTemporalFecha` on the `usuario` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `usuario` DROP COLUMN `eliminadoTemporalFecha`,
-    ADD COLUMN `eliminado_temporal_fecha` DATETIME(3) NULL;
