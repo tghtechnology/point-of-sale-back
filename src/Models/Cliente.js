56ac75e8e8deace62f04390ed227cf48725320e2
Estefany Torres <estefanyt3101@gmail.com>
2024-03-25 08:58:15 -0500

    Cliente

---
+++ src/Models/Cliente.js
@@ -0,0 +1,15 @@
+class CLiente{
+    constructor(id, nombre, correo, telefono, direccion, ciudad, region, codigo_postal, pais, estado){
+        this.id = id
+        this.nombre = nombre
+        this.correo = correo
+        this.telefono = telefono
+        this.direccion = direccion
+        this.ciudad = ciudad
+        this.region = region
+        this.codigo_postal = codigo_postal
+        this.pais = pais
+        this.estado = estado
+    }
+}
+export default CLiente
