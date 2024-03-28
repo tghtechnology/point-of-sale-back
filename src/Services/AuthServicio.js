7127e4e97f5b70a1bcb2c38a47d86f508cf62ace
DominickPortella <dominickportella1470@gmail.com>
2024-03-27 23:49:36 -0500

    new

--- src/Services/AuthServicio.js
+++ src/Services/AuthServicio.js
@@ -1,238 +1,219 @@
-import { connect } from "../database";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
-import getVerificationEmailTemplate from "../helpers/helperPlantilla";
 import { PrismaClient } from "@prisma/client";
+import { connect } from "../database";
+import getVerificationEmailTemplate from "../helpers/helperPlantilla";
+
 const prisma = new PrismaClient();
 
-//Lógica para iniciar sesión
+// Lógica para iniciar sesión
 export const login = async (email, password) => {
     const connection = await connect();
     const results = await prisma.usuario.findMany({
-      where: {
-        email: email
-      },
-    })
+        where: {
+            email: email
+        }
+    });
+    
     if (results.length === 0) {
-      throw new Error("Nombre de usuario o contraseña incorrectos");
+        throw new Error("Nombre de usuario o contraseña incorrectos");
     }
 
     const usuario = results[0];
-
     const match = await bcrypt.compare(password, usuario.password);
+    
     if (!match) {
-      throw new Error("Nombre de usuario o contraseña incorrectos");
+        throw new Error("Nombre de usuario o contraseña incorrectos");
     }
-    //Verificar si existe una sesión activa 
+
     const existingSessions = await prisma.sesion.findMany({
-      where: {
-        usuario_id: usuario.id
-      },
-    })
+        where: {
+            usuario_id: usuario.id
+        }
+    });
+
     if (existingSessions.length > 0) {
-      throw new Error("Sesión activa encontrada");
+        throw new Error("Sesión activa encontrada");
     }
     
     const token = jwt.sign(
-      { id: usuario.id, email: usuario.email },
-      "secreto_del_token",
-      { expiresIn: "24h" }
+        { id: usuario.id, email: usuario.email },
+        "secreto_del_token",
+        { expiresIn: "24h" }
     );
 
     const expiracion = new Date();
     expiracion.setHours(expiracion.getHours() + 24);
-    const result=await prisma.sesion.create({
-      data: {
-        usuario_id: usuario.id,
-        token: token,
-        expiracion: expiracion
-      }
-    })
+    
+    const result = await prisma.sesion.create({
+        data: {
+            usuario_id: usuario.id,
+            token: token,
+            expiracion: expiracion
+        }
+    });
+
     return {
-      usuario_id: result.usuario_id,
-      token: result.token
-    }
+        usuario_id: result.usuario_id,
+        token: result.token
+    };
 };
 
 // Lógica para cerrar sesión
 export const logout = async (req, res) => {
-  try {
-    // Extraer el token del encabezado de autorización
     const token = req.headers.authorization.split(' ')[1];
-    
-    // Decodificar el token
     const decodedToken = jwt.verify(token, "secreto_del_token");
     
-    // Eliminar el token de sesión del usuario de la base de datos
     await prisma.sesion.deleteMany({
-      where: {
-        usuario_id: decodedToken.id,
-        token: token,
-      },
+        where: {
+            usuario_id: decodedToken.id,
+            token: token
+        }
     });
 
-    // Enviar una respuesta exitosa
     res.status(200).json({ mensaje: "Sesión cerrada exitosamente" });
-  } catch (error) {
-    // Manejar errores
-    console.error('Error al cerrar sesión:', error);
-    res.status(500).json({ mensaje: 'Error interno del servidor' });
-  }
 };
-
 
 // Función para enviar un correo electrónico al usuario con un enlace para cambiar la contraseña
 export const enviarCorreoCambioPass = async (email) => {
-    // Verificar si el correo electrónico existe en la base de datos
-    const usuario=await prisma.usuario.findUnique({
-      where: {
-        email:email,
-      },
-      select:{
-        id:true,
-        nombre:true
-      }
+    const usuario = await prisma.usuario.findUnique({
+        where: { email: email },
+        select: { id: true, nombre: true }
     });
-    if(!usuario){
-      throw new Error("Correo no encontrado");
+
+    if (!usuario) {
+        throw new Error("Correo no encontrado");
     }
-    // Generar un token para el cambio de contraseña
+
     const token = jwt.sign(
-      { usuarioId: usuario.id, email },
-      "secreto_del_token_para_cambio_password",
-      { expiresIn: "1h" }
+        { usuarioId: usuario.id, email },
+        "secreto_del_token_para_cambio_password",
+        { expiresIn: "1h" }
     );
 
-    //Creacion de registro en resetTokens
     const expiracion = new Date();
     expiracion.setHours(expiracion.getHours() + 1);
+
     await prisma.resetToken.create({
-      data: {
-        token: token,
-        expiracion: expiracion, 
-        usuario_id: usuario.id,
-      },
+        data: {
+            token: token,
+            expiracion: expiracion,
+            usuario_id: usuario.id
+        }
     });
 
-    // Generar el enlace para cambiar la contraseña
     const resetPasswordLink = `http://${process.env.URL}/cambiar?token=${token}`;
- 
-    // Configurar el transporte de correo electrónico
+    
     const transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
     });
 
-    // Enviar el correo electrónico con el enlace para cambiar la contraseña
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Cambio de Contraseña",
-      html: getVerificationEmailTemplate(usuario.nombre, resetPasswordLink),
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Cambio de Contraseña",
+        html: getVerificationEmailTemplate(usuario.nombre, resetPasswordLink)
     });
-
 };
 
 // Cambiar la contraseña del usuario a través de un enlace con token
 export const cambiarPassword = async (token, password) => {
-  // Verificar si el token está vacío
   if (!token) {
       throw new Error("Falta el token");
   }
 
-  // Descodificar el token
-  const decodedToken = jwt.verify(
-      token,
-      "secreto_del_token_para_cambio_password"
-  );
-    //Verificacion de token
+  const decodedToken = jwt.verify(token, "secreto_del_token_para_cambio_password");
   const resetToken = await prisma.resetToken.findFirst({
       where: {
           token: token,
-          expiracion: {
-              gt: new Date(),
-          },
-      },
+          expiracion: { gt: new Date() }
+      }
   });
 
   if (!resetToken) {
       throw new Error("El token no es válido o ha expirado");
   }
-  //Busqueda del usuario
+
   const usuario = await prisma.usuario.findUnique({
-      where: {
-          id: resetToken.usuario_id,
-      },
+      where: { id: resetToken.usuario_id }
   });
 
   if (!usuario) {
       throw new Error("Usuario no encontrado");
   }
 
-  // Encriptar la nueva contraseña
   const hashedNewPassword = await bcrypt.hash(password, 10);
 
-  // Actualizar la contraseña del usuario en la base de datos
   await prisma.usuario.update({
-      where: {
-          id: resetToken.usuario_id,
-      },
-      data: {
-          password: hashedNewPassword,
-      },
+      where: { id: resetToken.usuario_id },
+      data: { password: hashedNewPassword }
   });
 
-  // Eliminar el token usado de la base de datos después de cambiar la contraseña
   await prisma.resetToken.deleteMany({
-      where: {
-          usuario_id: resetToken.usuario_id,
-      },
+      where: { usuario_id: resetToken.usuario_id }
   });
 
-  // Verificar si el usuario tiene una sesión activa
   const activeSessions = await prisma.sesion.findMany({
       where: {
           usuario_id: usuario.id,
-          expiracion: {
-              gt: new Date(),
-          },
-      },
+          expiracion: { gt: new Date() }
+      }
   });
 
-  // Si el usuario tiene una sesión activa, cerrarla
   if (activeSessions.length > 0) {
       await logout(activeSessions[0].token);
   }
+
+  // Devolver un mensaje de éxito
+  return "Cambio de contraseña exitoso";
 };
+
 
 // Función para eliminar tokens de sesión expirados y tokens de cambio de contraseña expirados de la base de datos
 export const eliminarTokensExpirados = async () => {
-    const db = await connect();
     const eliminarTokenSesion = await prisma.resetToken.deleteMany({
-      where: {
-        expiracion: {
-          lt: new Date(),
-        },
-      },
-    }
-    );
-
-    const eliminarTokenPassword = await prisma.sesion.deleteMany({
-      where: {
-        expiracion: {
-          lt: new Date(),
-        },
-      },
+        where: { expiracion: { lt: new Date() } }
     });
 
-    if (
-      eliminarTokenSesion.affectedRows > 0 ||
-      eliminarTokenPassword.affectedRows > 0
-    ) {
-      console.log("Tokens expirados eliminados correctamente.");
+    const eliminarTokenPassword = await prisma.sesion.deleteMany({
+        where: { expiracion: { lt: new Date() } }
+    });
+
+    if (eliminarTokenSesion.affectedRows > 0 || eliminarTokenPassword.affectedRows > 0) {
+        console.log("Tokens expirados eliminados correctamente.");
     }
 };
+
+// Función para verificar contraseña antes de eliminar
+export const verificarContrasena = async (id, password) => {
+    const passwordFromRequest = password;
+    const contrasena = await prisma.usuario.findUnique({
+        where: {
+            id: parseInt(id),
+            estado: true
+        }
+    });
+
+    if (!contrasena) {
+        return null;
+    }
+
+    const hashedPasswordFromDatabase = contrasena.password;
+    const match = await bcrypt.compare(passwordFromRequest, hashedPasswordFromDatabase);
+
+    return match;
+};
+
+module.exports = {
+  login,
+  logout,
+  enviarCorreoCambioPass,
+  cambiarPassword,
+  eliminarTokensExpirados,
+  verificarContrasena 
+}
