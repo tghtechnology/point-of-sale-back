0c11d51cd64baecad0fde2d629774922ddf1a7ed
JimenaAgustinNavarro <jimenaagustin0@gmail.com>
2024-03-29 19:13:51 -0500

    cambio app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import routerArticulo from "./Routes/ArticuloRoute"
 import routerCategoria from "./Routes/CategoriaRoute"
 import routerCliente from "./Routes/ClienteRouter"
 import routerEmpleado from "./Routes/EmpleadoRouter"
+import routerImpuesto from "./Routes/ImpuestoRouter"
 import morgan from "morgan";
 
 import swaggerJSDoc from "swagger-jsdoc";
@@ -27,6 +28,7 @@ app.use(routerCategoria)
 app.use(routerAuth)
 app.use(routerCliente)
 app.use(routerEmpleado)
+app.use(routerImpuesto)
 
 app.use('/docs',swaggerui.serve,swaggerui.setup(specs));
 export default app
