ca74e51bc8c17e9611a09d6b296faba81c81ed8f
Estefany Torres <estefanyt3101@gmail.com>
2024-03-30 02:09:04 -0500

    new

--- src/Services/UsuarioServicio.js
+++ src/Services/UsuarioServicio.js
@@ -24,8 +24,48 @@ export const crearUsuario = async (nombre, email, password, pais) => {
     return newUsuario
 }; 
 
-//Eliminar temporalmente una cuenta
-export const eliminarTemporalmente = async (id, password,token) => {
+const validarUsuario = async (id, password, token) => {
+  const usuario = await prisma.usuario.findUnique({
+      where: {
+          id: parseInt(id),
+          estado: true
+      },
+      select: {
+          password: true
+      }
+  });
+
+  if (!usuario) {
+      throw new Error("Usuario no encontrado");
+  }
+
+  const match = await bcrypt.compare(password, usuario.password);
+  if (!match) {
+      throw new Error("Contraseña incorrecta");
+  }
+
+  if (token) {
+      const sesionValida = await prisma.sesion.findFirst({
+          where: {
+              token
+          }
+      });
+      if (!sesionValida) {
+          throw new Error("Debe iniciar sesión");
+      }
+  }
+
+  return usuario;
+};
+const eliminarSesionesActivas = async (usuarioId) => {
+  const activeSessions = await prisma.sesion.findMany({
+      where: { usuario_id: usuarioId, expiracion: { gt: new Date() } }
+  });
+
+  if (activeSessions.length > 0) await logout(activeSessions[0].token);
+};
+
+export const eliminarTemporalmente = async (id, password, token) => {
   const usuarioverificado= await prisma.usuario.findUnique({
     where: {
       id: parseInt(id),
@@ -35,141 +75,66 @@ export const eliminarTemporalmente = async (id, password,token) => {
   if(usuarioverificado){
     throw new Error("Cuenta eliminada");
   }
-  const sesionValida = await prisma.sesion.findFirst({
-    where: {
-      token: token
-    }
-  });
-  if (!sesionValida) {
-    throw new Error("Debe iniciar sesión");
-  }
-  const usuario = await prisma.usuario.findUnique({
-    where: {
-      id: parseInt(id),
-      estado: true
-    },
-    select: {
-      password: true
-    }
-  });
-
-  if (!usuario) {
-    throw new Error("Usuario no encontrado");
-  }
-
-  const match = await bcrypt.compare(password, usuario.password);
-  if (!match) {
-    throw new Error("Contraseña incorrecta");
-  }
-
+  const usuario = await validarUsuario(id, password, token);
   const results = await prisma.usuario.update({
-    where: { id: parseInt(id) },
-    data: {
-      estado: false,
-      eliminado_temporal_fecha: { set: new Date() }
-    }
+      where: { id: parseInt(id) },
+      data: {
+          estado: false,
+          eliminado_temporal_fecha: { set: new Date() }
+      }
   });
-  // Verificar si el usuario tiene una sesión activa
-  const activeSessions = await prisma.sesion.findMany({
-    where: {
-        usuario_id: usuario.id,
-        expiracion: {
-            gt: new Date(),
-        },
-    },
-});
-// Si el usuario tiene una sesión activa, cerrarla
-  if (activeSessions.length > 0) {
-      await logout(activeSessions[0].token);
-  }
-    return results;
-};
-
-//Eliminar permanente
-export const eliminarPermanentemente = async (id,password,token) => {
-  const sesionValida = await prisma.sesion.findFirst({
-    where: {
-      token: token
-    }
-  });
-  if (!sesionValida) {
-    throw new Error("Debe iniciar sesión");
-  }
-  const usuario = await prisma.usuario.findUnique({
-    where: {
-      id: parseInt(id),
-      estado: true
-    },
-    select: {
-      password: true
-    }
-  });
-
-  if (!usuario) {
-    throw new Error("Usuario no encontrado");
-  }
-
-  const match = await bcrypt.compare(password, usuario.password);
-  if (!match) {
-    throw new Error("Contraseña incorrecta");
-  }
-  const activeSessions = await prisma.sesion.findMany({
-    where: {
-        usuario_id: usuario.id,
-        expiracion: {
-            gt: new Date(),
-        },
-    },
-  });
-  // Si el usuario tiene una sesión activa, cerrarla
-  if (activeSessions.length > 0) {
-    await logout(activeSessions[0].token);
-  }
-  const results = await prisma.usuario.delete({ where: { id: parseInt(id) } });
+  await eliminarSesionesActivas(usuario.id);
 
   return results;
 };
 
-//Eliminicar cuentas temporales pasado el tiempo límite
+export const eliminarPermanentemente = async (id, password, token) => {
+  const usuario=await validarUsuario(id, password, token);
+  await eliminarSesionesActivas(usuario.id);
+  const results = await prisma.usuario.delete({ where: { id: parseInt(id) } });
+  return results;
+};
+
 export const eliminarCuentasVencidas = async (id) => {
   const fechaUnaSemanaAtras = new Date();
   fechaUnaSemanaAtras.setDate(fechaUnaSemanaAtras.getDate() - 7);
 
-  const usuarioEliminado = await prisma.usuario.findFirst({
-    where: {
-      id: parseInt(id),
-      estado: false,
-      eliminado_temporal_fecha: { lte: fechaUnaSemanaAtras }
-    }
+  const results = await prisma.usuario.deleteMany({
+      where: {
+          id: parseInt(id),
+          estado: false,
+          eliminado_temporal_fecha: { lte: fechaUnaSemanaAtras }
+      }
   });
 
-  if (!usuarioEliminado) return false;
-
-  const results = await prisma.usuario.delete({ where: { id: parseInt(id) } });
-  return results;
+  return results.count > 0;
 };
 
-//Restaurar cuentas 
 export const restaurarCuenta = async (id) => {
   const usuario = await prisma.usuario.findUnique({
-    where: { id: parseInt(id) },
-    select: {
-      id: true,
-      estado: true,
-      eliminado_temporal_fecha: true
-    }
+      where: { id: parseInt(id) },
+      select: {
+          id: true,
+          estado: true,
+          eliminado_temporal_fecha: true
+      }
   });
 
-  if (!usuario || usuario.estado === 1) return null;
+  if (!usuario || usuario.estado === 1) {
+      return null;
+  }
 
   const unaSemanaEnMiliseg = 7 * 24 * 60 * 60 * 1000;
   const fechaEliminacion = new Date(usuario.eliminado_temporal_fecha);
 
-  if (Date.now() - fechaEliminacion > unaSemanaEnMiliseg) return true;
+  if (Date.now() - fechaEliminacion > unaSemanaEnMiliseg) {
+      return true;
+  }
 
   const results = await prisma.usuario.update({
-    where: { id: parseInt(id) },
-    data: { estado: true, eliminado_temporal_fecha: null }
+      where: { id: parseInt(id) },
+      data: { estado: true, eliminado_temporal_fecha: null }
   });
+
   return results;
 };
