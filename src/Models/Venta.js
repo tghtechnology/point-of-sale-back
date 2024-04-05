28e4a0f1db197e4139c6f539d1545f28efa94b6c
Estefany Torres <estefanyt3101@gmail.com>
2024-04-05 13:15:09 -0500

    Venta

---
+++ src/Models/Venta.js
@@ -0,0 +1,10 @@
+class Venta {
+    constructor(detalles, tipoPago, impuestoId, descuentoId, subtotal, total){
+        this.detalles = detalles;
+        this.tipoPago = tipoPago;
+        this.impuestoId = impuestoId;
+        this.descuentoId = descuentoId;
+        this.subtotal = subtotal;
+        this.total = total;
+    }
+}
