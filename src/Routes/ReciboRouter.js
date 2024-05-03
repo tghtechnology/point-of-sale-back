50ed9cc4aae8cd81ad4d96beadd01d05f8d1cfd1
JimenaAgustinNavarro <jimenaagustin0@gmail.com>
2024-05-03 13:46:10 -0500

    proteger rutas venta y recibo

--- src/Routes/ReciboRouter.js
+++ src/Routes/ReciboRouter.js
@@ -1,11 +1,12 @@
 import { crearRecibo, ListarRecibo,Reembolsar, ListarReciboById, ListarRecibosByVenta } from "../controllers/ReciboControlador";
 import { Router } from "express";
+import { verificarAuth, isPropietario } from "../Middleware/verificarAuth";
 
 const routerRecibo =Router()
 
 //routerRecibo.post("/recibo/:id", crearRecibo)
-routerRecibo.get("/recibo", ListarRecibo)
-routerRecibo.post("/reembolsar",Reembolsar)
-routerRecibo.get("/recibo/:id",ListarReciboById)
-routerRecibo.get("/recibo/venta/:id_venta",ListarRecibosByVenta)
+routerRecibo.get("/recibo", verificarAuth, isPropietario, ListarRecibo)
+routerRecibo.post("/reembolsar", verificarAuth, isPropietario, Reembolsar)
+routerRecibo.get("/recibo/:id",verificarAuth, isPropietario, ListarReciboById)
+routerRecibo.get("/recibo/venta/:id_venta", verificarAuth, isPropietario, ListarRecibosByVenta)
 export default routerRecibo
--- src/Routes/VentaRouter.js
+++ src/Routes/VentaRouter.js
@@ -1,11 +1,12 @@
 import { CrearVenta, ListarVentas,ObtenerVentaPorId } from "../controllers/VentaControlador";
 import { Router } from "express";
+import { verificarAuth, isPropietario } from "../Middleware/verificarAuth";
 
 const routerVenta =Router()
 
-routerVenta.post("/venta", CrearVenta)
-routerVenta.get("/venta", ListarVentas)
-routerVenta.get("/venta/:id",ObtenerVentaPorId)
+routerVenta.post("/venta", verificarAuth, isPropietario, CrearVenta)
+routerVenta.get("/venta", verificarAuth, isPropietario, ListarVentas)
+routerVenta.get("/venta/:id", verificarAuth, isPropietario, ObtenerVentaPorId)
 
 
 export default routerVenta
