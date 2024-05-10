2b8de17cf9200770f43f8637a9fdae78d91804b6
Estefany Torres <estefanyt3101@gmail.com>
2024-05-09 20:51:17 -0500

    New

--- src/Utils/cloudinary.js
+++ src/Utils/cloudinary.js
@@ -1,7 +1,7 @@
 import {v2 as cloudinary} from 'cloudinary';
 
 cloudinary.config({ 
-    cloud_name: 'dusghtj8l', 
+    cloud_name: 'dnwzliif9', 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
