9820df8253ac681454de8411c0b2333de8a7df3f
DominickPortella <157386679+DominickPortella@users.noreply.github.com>
2024-06-14 10:56:51 -0500

    new

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
 };
 export default config;
