fe670b1881816f39b6765e214a4307d261282a90
Estefany Torres <estefanyt3101@gmail.com>
2024-06-18 13:30:22 -0500

    New

--- prisma/migrations/20240604163854_sistema_venta/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `usuario` MODIFY `nombre` VARCHAR(255) NULL,
-    MODIFY `cargo` VARCHAR(255) NULL,
-    MODIFY `telefono` VARCHAR(255) NULL;
--- prisma/migrations/20240618144312_sistema_venta/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `precio` on table `articulo` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `articulo` MODIFY `precio` DECIMAL(10, 2) NOT NULL;
